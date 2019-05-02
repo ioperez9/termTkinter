f83aed07bbe5cb3da8d4c4599cd24aa280bce761
Inma <Inma@Inma.local>
2019-05-02 12:51:49 +0200

    TermTkinter.Creación objetos.Radiobutton

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ class mainApp(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.title("Termometro")
-        self.geometry("200x150")
-        self.configure(bg = "#DED789")
+        self.geometry("210x150")
+        self.configure(bg = "#ECECEC")
+        #para que no puedas reescalar la ventana ponemos esto y será fijo el tamaño que pongamos
+        self.resizable(0,0)
         
         self.temperatura = StringVar(value="")
         self.tipoUnidad = StringVar(value="C")
@@ -17,8 +19,10 @@ class mainApp(Tk):
         self.createLayout()
     
     def createLayout(self):
-        self.entrada = ttk.Entry(self, variable=self.temperatura)
-        
+        self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)
+        self.lblUnidad = ttk.Label(self, text="Grados").place(x=10, y=45)
+        self.rb1 = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F").place(x=20, y=70)
+        self.rb2 = ttk.Radiobutton(self, text="Celsius", variable=self.tipoUnidad, value="C").place(x=20, y=95)
 
         
     def start(self):
