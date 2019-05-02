8ebd2a58e72a6cc310d1d55db9f8cfdcab2b98e7
Inma <Inma@Inma.local>
2019-05-02 18:15:05 +0200

    TermTkinter. Función selected

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class mainApp(Tk):
     entrada = None
     tipoUnidad = None
     
+    __temperaturaAnt = ""
+    
     def __init__(self):
         Tk.__init__(self)
         self.title("Termometro")
@@ -21,9 +23,10 @@ class mainApp(Tk):
     
     def createLayout(self):
         self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)
+        
         self.lblUnidad = ttk.Label(self, text="Grados").place(x=10, y=45)
-        self.rb1 = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F").place(x=20, y=70)
-        self.rb2 = ttk.Radiobutton(self, text="Celsius", variable=self.tipoUnidad, value="C").place(x=20, y=95)
+        self.rb1 = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F", command=self.selected).place(x=20, y=70)
+        self.rb2 = ttk.Radiobutton(self, text="Celsius", variable=self.tipoUnidad, value="C", command=self.selected).place(x=20, y=95)
 
         
     def start(self):
@@ -31,7 +34,29 @@ class mainApp(Tk):
         
     
     def validateTemperature(self, *args):
-        print(self.temperatura.get())
+        nuevoValor = self.temperatura.get()
+        print("nuevoValor", nuevoValor, "vs valorAnterior ", self.__temperaturaAnt)
+        try:
+            float(nuevoValor)
+            self.__temperaturaAnt = nuevoValor
+            print("fija valor anterior a: ", self.__temperaturaAnt)
+        except:
+            self.temperatura.set(self.__temperaturaAnt)
+            print("recupera valor anterior ", self.__temperaturaAnt)
+            
+    def selected(self):
+        resultado= 0
+        toUnidad = self.tipoUnidad.get()
+        grados = float(self.temperatura.get())
+        
+        if toUnidad == "F":
+            resultado = grados * 9/5 + 32
+        elif toUnidad == "C":
+            resultado = (grados - 32) * 5/9
+        else:
+            resultado = grados
+        
+        self.temperatura.set(resultado)
         
         
 if __name__ == "__main__":
