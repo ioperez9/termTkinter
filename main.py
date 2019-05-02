66459be53e57d21b47def9831f1372fb31f787cc
Inma <Inma@Inma.local>
2019-05-02 12:23:48 +0200

    TermTkinter. Controles

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ from tkinter import *
 from tkinter import ttk
 
 class mainApp(Tk):
+    entrada = None
+    tipoUnidad = None
     
     def __init__(self):
         Tk.__init__(self)
         self.title("Termometro")
         self.geometry("200x150")
         self.configure(bg = "#DED789")
+        
+        self.temperatura = StringVar(value="")
+        self.tipoUnidad = StringVar(value="C")
+        
+        self.createLayout()
+    
+    def createLayout(self):
+        self.entrada = ttk.Entry(self, variable=self.temperatura)
+        
+
         
     def start(self):
         self.mainloop()
